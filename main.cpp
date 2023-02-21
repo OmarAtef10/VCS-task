1c76b5452a1a5a82b713cbd05d0cdb4461e9fb20
Omar Atef <20190356@stud.fci-cu.edu.eg>
2023-02-21 23:30:26 +0200

    average

--- main.cpp
+++ main.cpp
@@ -13,10 +13,17 @@ int get_sum(vector<int> &nums) {
     return sum;
 }
 
+int get_average(vector<int> &nums) {
+    int sum = get_sum(nums);
+    return sum / nums.size();
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = get_sum(nums);
+    int average = get_average(nums);
     cout << "Sum of Vector is " << sum << endl;
+    cout << "Average of Vector is " << average << endl;
     return 0;
 }
