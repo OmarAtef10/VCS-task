96daf71f1d77143e6a7f1fc9cb2f1e90f3fd6b69
Omar Atef <20190356@stud.fci-cu.edu.eg>
2023-02-21 23:33:37 +0200

    min

--- main.cpp
+++ main.cpp
@@ -13,10 +13,22 @@ int get_sum(vector<int> &nums) {
     return sum;
 }
 
+int get_min(vector<int> &nums) {
+    int min = nums[0];
+    vector<int>::iterator i;
+    for (i = nums.begin(); i < nums.end(); i++) {
+        if (*i < min) {
+            min = *i;
+        }
+    }
+    return min;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     vector<int> nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int sum = get_sum(nums);
     cout << "Sum of Vector is " << sum << endl;
+    cout<< "Min of Vector is " << get_min(nums) << endl;
     return 0;
 }
